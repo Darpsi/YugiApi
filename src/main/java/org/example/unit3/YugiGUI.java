885cbff0aa1357cf8a35c7a95db79d73f5721bdf
briangonzalez2 <160563414+briangonzalez2@users.noreply.github.com>
2025-10-04 19:57:36 -0500

    Revert "Merge remote-tracking branch 'origin/diego' into brian"
    This reverts commit 723fd824318d722dfc468d3601a8bd7437f896dd, reversing
    changes made to 67c89d00249f5c0250a8743f00d4384ee2ef7299.

--- src/main/java/org/example/unit3/YugiGUI.java
+++ src/main/java/org/example/unit3/YugiGUI.java
@@ -1,9 +1,6 @@
 package org.example.unit3;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Random;
 
 public class YugiGUI {
     private JPanel mainPanel;
@@ -28,29 +25,6 @@ public class YugiGUI {
     private JButton usarButton1;
     private JButton usarButton3;
     
-
-
-    public YugiGUI() {
-        RandomUp.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        RandomDown.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-    }
-
-
-    public int generarCarta() {
-        Random rand = new Random();
-        int numero = rand.nextInt(8000000)+1;
-        return numero;
-    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("YugiGUI");
